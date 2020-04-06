253f0e6af8c4e2c99f21ab48fce4f654e0ee0a05
darkblue5 <xzdo@qq.com>
2020-04-06 16:34:39 +0800

    增减函数内容
    清空一个函数的内容，增加onShow

--- vocIndex.js
+++ vocIndex.js
@@ -11,30 +11,12 @@ Page({
   },
 
   onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
-          })
-        }
-      }
-    })
+        // i delete these codes
   },
+
+    onshow: function(e) {
+        console.log('page on show');
+    },
 
   onGetUserInfo: function(e) {
     if (!this.data.logged && e.detail.userInfo) {
